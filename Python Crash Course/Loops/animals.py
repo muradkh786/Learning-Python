b8c5c85e93bd82de06b59b89e2190307ddcbec2b
Murad Khan <murad.kh786@gmail.com>
2020-08-02 16:11:02 -0400

    Added more for loop exercises

---
+++ Python Crash Course/Loops/animals.py
@@ -0,0 +1,4 @@
+birds = ['conure', 'cockatiel', 'parakeet']
+for bird in birds:
+    print('I would love to have a ' + bird)
+print('All of these birds are considered parrots!')
---
+++ Python Crash Course/Loops/pizza.py
@@ -0,0 +1,5 @@
+pizzas = ['cheese', 'jalapeno', 'chicken']
+for pizza in pizzas:
+    print("I like to eat " + pizza + " pizza.\n")
+
+print("I really love pizza!")
