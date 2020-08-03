c33338b274acfcbfe839d32652f95a1912bd8714
Murad Khan <murad.kh786@gmail.com>
2020-08-02 21:05:42 -0400

    Update exercisesChap4-3to4-9.py

--- Python Crash Course/Loops/exercisesChap4-3to4-9.py
+++ Python Crash Course/Loops/exercisesChap4-3to4-9.py
@@ -1,9 +1,40 @@
 # 4-3
+# Counting to Twenty
 # for value in range(1,21):
 #     print(value)
 
-#4-4
+# 4-4
+# One Million
 # oneMillion = list(range(1,1000001))
 # for value in oneMillion:
 #     print(value)
 
+# 4-5
+# Sum of 1 to a Million
+# oneMillion = list(range(1,1000001))
+# print(min(oneMillion))
+# print(max(oneMillion))
+# print(sum(oneMillion))
+
+# 4-6
+# Odd Numbers
+# for value in range(1,21,2):
+#     print(value)
+
+# 4-7
+# Multiple of Threes
+# threes = []
+# for value in range(3,31,3):
+#     threes.append(value)
+# for three in threes:
+#     print(three)
+
+# 4-8
+# Cubes
+# cubes = [value**3 for value in range(1,11)]
+# for cube in cubes:
+#     print(cube)
+
+# 4-9
+# Cube Comprehension
+# See 4-8
