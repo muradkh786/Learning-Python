7183d7abb77212f25e8bf3b3f745b08b46a0a1a3
Murad Khan <murad.kh786@gmail.com>
2020-08-02 20:53:32 -0400

    Create exercisesChap4-3to4-9.py

---
+++ Python Crash Course/Loops/exercisesChap4-3to4-9.py
@@ -0,0 +1,12 @@
+# 4-3
+# print("Exercise 4-3")
+# for value in range(1,21):
+#     print(value)
+# print("\n")
+
+#4-4
+# print("Exercise 4-4")
+# oneMillion = list(range(1,1000001))
+# for value in oneMillion:
+#     print(value)
+# print("\n")
