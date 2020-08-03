aefc442a40ef4494bc9b46cb4bd8eaca069963df
Murad Khan <murad.kh786@gmail.com>
2020-08-02 21:11:34 -0400

    Slice examples

---
+++ Python Crash Course/Loops/players.py
@@ -0,0 +1,6 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+
+# you can omit either ends to either start at the beginning or end at the end
+print(players[:4])
+print(players[2:])
